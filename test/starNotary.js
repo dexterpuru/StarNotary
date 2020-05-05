db6a1453ef2d2461c9ae4fca9a6812a091a38379
Pratyaksh <pratyaksh2000saini@gmail.com>
2020-05-06 03:19:37 +0530

    test file updated

--- test/starNotary.js
+++ test/starNotary.js
@@ -17,3 +17,21 @@ it("has correct name", async () => {
   let starName = await instance.starName.call(); // Calling the starName property
   assert.equal(starName, "Awesome Udacity Star"); // Assert if the starName property was initialized correctly
 });
+
+it("can be claimed", async () => {
+  let instance = await StarNotary.deployed(); // Making sure the Smart Contract is deployed and getting the instance.
+  await instance.claimStar({ from: owner }); // Calling the Smart Contract function claimStar
+  let starOwner = await instance.starOwner.call(); // Getting the owner address
+  assert.equal(starOwner, owner); // Verifying if the owner address match with owner of the address
+});
+
+it("can change owners", async () => {
+  let instance = await StarNotary.deployed();
+  let secondUser = accounts[1];
+  await instance.claimStar({ from: owner });
+  let starOwner = await instance.starOwner.call();
+  assert.equal(starOwner, owner);
+  await instance.claimStar({ from: secondUser });
+  let secondOwner = await instance.starOwner.call();
+  assert.equal(secondOwner, secondUser);
+});
